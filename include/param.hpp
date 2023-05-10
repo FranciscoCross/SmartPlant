4d9baab61c860ef8c8dab43103116195560222e3
FeedehC <feedehc@gmail.com>
2023-05-10 10:24:55 -0300

    test: inserting error for pio check to fail

--- include/param.hpp
+++ include/param.hpp
@@ -5,7 +5,7 @@ param.hpp: se configuran los parámetros del dispositivo
 #define param_hpp
 
 // Version del Firmware
-#define FIRMWARE_VERSION 0.53
+#define FIRMWARE_VERSION 0.54
 
 // Actualizaciones de Firmware con autoupdate
 #define UPDATE_JSON_URL "https://firmware.power-pot.com/update/update.json"
