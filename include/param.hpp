40d32b7218ec8c89a41f534668824e40ec4c5d6e
Francisco De la Cruz <francisco.delacruz@craftech.io>
2023-12-11 20:41:38 -0300

    chore: change version

--- include/param.hpp
+++ include/param.hpp
@@ -5,7 +5,7 @@ param.hpp: se configuran los parámetros del dispositivo
 #define param_hpp
 
 // Version del Firmware
-#define FIRMWARE_VERSION 0.60
+#define FIRMWARE_VERSION 0.61
 
 // Actualizaciones de Firmware con autoupdate
 #define UPDATE_JSON_URL_DEV "https://firmware-dev.power-pot.com/update/update.json"
